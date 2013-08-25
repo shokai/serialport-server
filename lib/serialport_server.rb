b0628e1dd156c3a245ddcf8545d40c5aab0a66ec
Wilco van Duinkerken <wilco@sparkboxx.com>
2013-08-25 13:04:45 +0200

    move class definitions from bin to lib

---
+++ lib/serialport_server.rb
@@ -0,0 +1,8 @@
+require 'eventmachine'
+require 'evma_httpserver'
+require 'em-websocket'
+require 'json'
+
+require_relative 'serialport_server/version'
+require_relative 'serialport_server/serial_socket_server'
+require_relative 'serialport_server/serial_http_server'
